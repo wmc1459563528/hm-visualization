26339bd6c1c64092f01395752e9f13645edd987f
A Lucky Boy <1459563528@qq.com>
2022-07-10 12:22:51 +0800

    注册页完成

--- js/common.js
+++ js/common.js
@@ -5,3 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+// 轻提示
+const myBox = document.querySelector('#myToast')
+const toast = new bootstrap.Toast(myBox, {
+  animation: true, // 开启过渡动画
+  autohide: true, // 开启自动隐藏
+  delay: 3000, // 3s后自动关闭
+})
+// 将提示信息封装函数进行传参
+const showTip = res => {
+  // 获取到提示信息并修改
+  myBox.querySelector('.toast-body').innerHTML = res
+  toast.show()
+}
+//  axios请求根路径
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
