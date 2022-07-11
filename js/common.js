589e8f38592bb13cd26b2d09440b326ac3e64777
A Lucky Boy <1459563528@qq.com>
2022-07-11 08:49:31 +0800

    添加请求拦截器功能

--- js/common.js
+++ js/common.js
@@ -37,3 +37,26 @@ if (logout) {
     location.href = './login.html'
   })
 }
+// todo 拦截器配置 携带token才可以访问数据
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // console.log(config)
+  // todo 携带token访问数据 如果不带token信息 则会报错401 
+  config.headers.Authorization = localStorage.getItem('user-token')
+  return config; // ! 不能删
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
